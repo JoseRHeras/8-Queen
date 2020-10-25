aff9f3b3a43ab6fbf1787d69a4991e240560a1b9
JRHeras <jheras@yahoo.com>
2020-10-25 15:03:22 -0500

    Home Page was given the following: Models, Improved UI

--- blog/models.py
+++ blog/models.py
@@ -10,11 +10,24 @@ class article(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.title} article"
+        return f"{self.title} Article"
 
 class article_image(models.Model):
     article = models.OneToOneField(article, on_delete=models.CASCADE)
     image = models.ImageField(default="default.jpg", upload_to="article_pics")
 
     def __str__(self):
-        return f"{self.article.title}"
+        return f"{self.article.title} Image"
+
+class user_introduction_data(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    contact_email = models.EmailField(max_length=254)
+    github = models.URLField(max_length=200)
+    linkedin = models.URLField(max_length=200)
+    image = models.ImageField(default='default_user_pic.jpg', upload_to="user_pics")
+    slogan = models.TextField()
+    location = models.CharField(max_length=50)
+
+
+    def __str__(self):
+        return f"{self.user.first_name} Intro Card"
