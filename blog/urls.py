3aeea17fbe5de712ace490418b114e35c834ec46
JRHeras <jheras@yahoo.com>
2020-11-18 19:24:53 -0600

    Initial implementation of project app

--- blog/urls.py
+++ blog/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from . import views
 from .views import ArticleListView
 from article.views import FullArticleDetailView
+from project.views import projects
 
 urlpatterns = [
     path('', views.home, name="blog-home"),
     path('security/', ArticleListView.as_view(), name='blog-security'),
-    path('projects/', views.projects, name='blog-projects'),
+    # path('projects/', views.projects, name='blog-projects'),
     path('underconstruction/', views.under_construction, name='blog-underconstruction'),
-    path('full-article/<int:pk>/', FullArticleDetailView.as_view(), name='full-article-view')
+    path('full-article/<int:pk>/', FullArticleDetailView.as_view(), name='full-article-view'),
+    path('projects/', projects, name='blog-projects')
 ]
